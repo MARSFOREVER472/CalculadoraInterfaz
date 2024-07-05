00206322f00f00054c86a1d527e969b9898e83e3
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-07-05 13:23:01 -0400

    Se desplegaron los resultados!

---
+++ Calculadora/html/script.js
@@ -0,0 +1,27 @@
+function calculator()
+{
+    let num1 = parseFloat(document.getElementById('number1').value);
+    let num2 = parseFloat(document.getElementById('number2').value);
+
+    let operation = document.getElementById('operators').value;
+
+    if (operation === 'plus') // SI ES UNA SUMA...
+    {
+        document.getElementById('result').value = (num1 + num2); // DESPLIEGA EL RESULTADO DE LA SUMA.
+    }
+
+    if (operation === 'minus') // SI ES UNA RESTA...
+    {
+        document.getElementById('result').value = (num1 - num2); // DESPLIEGA EL RESULTADO DE LA RESTA.
+    }
+
+    if (operation === 'divide') // SI ES UNA DIVISIÓN...
+    {
+        document.getElementById('result').value = (num1 / num2).toFixed(3); // DESPLIEGA EL RESULTADO DE LA DIVISIÓN.
+    }
+
+    if (operation === 'multiplicate') // SI ES UNA MULTIPLICACIÓN...
+    {
+        document.getElementById('result').value = (num1 * num2); // DESPLIEGA EL RESULTADO DE LA MULTIPLICACIÓN.
+    }
+}
